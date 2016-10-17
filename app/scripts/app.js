3f19708e5b9670ce41f6d0286da4bfa6f5b002a6
耿林 <lin.geng@dinglicom.com>
2016-10-17 17:28:03 +0800

    'style'

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -46,19 +46,4 @@ angular
           }
         }
       })
-
-    //$routeProvider
-    //  .when('/', {
-    //    templateUrl: 'views/main.html',
-    //    controller: 'MainCtrl',
-    //    controllerAs: 'main'
-    //  })
-    //  .when('/about', {
-    //    templateUrl: 'views/about.html',
-    //    controller: 'AboutCtrl',
-    //    controllerAs: 'about'
-    //  })
-    //  .otherwise({
-    //    redirectTo: '/'
-    //  });
   });
--- app/scripts/controllers/index.controller.js
+++ app/scripts/controllers/index.controller.js
@@ -9,4 +9,15 @@ angular.module('taskApp')
           stuFn.param.isFinish = status ;
         }
       }
-})
+      $scope.items = [
+        {name: "全部课程", num: 1973, link:all},
+        {name: "文学艺术", num: 271, link:wxys},
+        {name: "哲学历史", num: 136, link:zxls},
+        {name: "经管法学", num: 180, link:jgfx},
+        {name: "基础科学", num: 943, link:jckx},
+        {name: "工程技术", num: 788, link:gcjs},
+        {name: "农林医药", num: 124, link:nlyx},
+        {name: "大学先修课", num: 31, link:dxxxk}
+      ];
+
+    })
