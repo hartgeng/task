fb8ddaeb1b1a1b3d1faeff5a544e5fffc7f6187d
耿林 <lin.geng@dinglicom.com>
2016-10-17 17:29:41 +0800

    'style'

--- app/scripts/controllers/index.controller.js
+++ app/scripts/controllers/index.controller.js
@@ -10,14 +10,14 @@ angular.module('taskApp')
         }
       }
       $scope.items = [
-        {name: "全部课程", num: 1973, link:all},
-        {name: "文学艺术", num: 271, link:wxys},
-        {name: "哲学历史", num: 136, link:zxls},
-        {name: "经管法学", num: 180, link:jgfx},
-        {name: "基础科学", num: 943, link:jckx},
-        {name: "工程技术", num: 788, link:gcjs},
-        {name: "农林医药", num: 124, link:nlyx},
-        {name: "大学先修课", num: 31, link:dxxxk}
+        {name: "全部课程", num: 1973},
+        {name: "文学艺术", num: 271},
+        {name: "哲学历史", num: 136},
+        {name: "经管法学", num: 180},
+        {name: "基础科学", num: 943},
+        {name: "工程技术", num: 788},
+        {name: "农林医药", num: 124},
+        {name: "大学先修课", num: 31}
       ];
 
     })
